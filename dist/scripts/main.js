b2dd8de35703dbc3a92f619c22f670fae918e8ad
Catherine Azzarello <azzcatdesign@gmail.com>
2021-08-14 15:00:34 -0700

    who can remember?

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-const toggler=document.querySelector(".menu__toggler"),menu=document.querySelector(".menu");toggler.addEventListener("click",()=>{toggler.classList.toggle("active"),menu.classList.toggle("active")});
+const toggler=document.querySelector(".menu__toggler"),menu=document.querySelector(".menu");toggler.addEventListener("click",(()=>{toggler.classList.toggle("active"),menu.classList.toggle("active")}));
